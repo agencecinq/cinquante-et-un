feb8e6de1a3727acb4c8aadfab503c76e23eed03
19h47 <jeremylevron@19h47.fr>
2025-02-21 16:42:54 +0100

    :sparkles: Multirow section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./**/*.liquid', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     container: false,
     extend: {
       screens: {
-        'prototype': '1440px',
+        prototype: '1440px',
       },
-    }
+    },
   },
   plugins: [],
 };
